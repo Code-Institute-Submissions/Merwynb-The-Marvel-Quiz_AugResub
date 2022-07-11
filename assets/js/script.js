197d394a6f1b6c37323615ca0982abf3ce5cf7b3
MERWYN BARRETTO <merwynbarretto@gmail.com>
2022-07-11 13:41:05 +0000

    Display question 1 to user via JS code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,25 @@
-function runGame() {
+
+document.addEventListener('DOMContentLoaded', function() {
+    let buttons = document.getElementsByTagName('button');
+
+    for (let button of buttons) {
+        button.addEventListener('click', function() {
+            let questionType = this.getAttribute('data-type');
+            runGame('questionType');
+        })
+    }
+
+    runGame('question1');
+})
+
+function runGame(questionType) {
+
+    if (questionType === 'question1') {
+        displayQuestion1();
+    } else {
+        alert(`Unknown question type: ${questionType}`);
+        throw `Unknown question type ${questionType}.Aborting!`;
+    }
 
 }
 
@@ -6,18 +27,25 @@ function checkAnswer() {
 
 }
 
-function incrementScore {
+function incrementScore() {
 
 }
 
-function displayQuestion1 {
+function displayQuestion1(question1, choice1, choice2, choice3) {
+
+    document.getElementById('question1').textContent = 'Who Plays Iron Man?';
+    document.getElementById('johnny').textContent = 'Johnny Depp';
+    document.getElementById('robert').textContent = 'Robert Downey Jr.';
+    document.getElementById('brad').textContent = 'Brad Pitt';
+    
+
 
 }
 
-function displayQuestion2 {
+function displayQuestion2() {
 
 }
 
-function displayQuestion3 {
+function displayQuestion3() {
     
 }
