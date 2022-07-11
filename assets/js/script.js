527573d31303d68d4fdbff6157dc2da181514d42
MERWYN BARRETTO <merwynbarretto@gmail.com>
2022-07-11 12:22:38 +0000

    Add styles and js structure for game functions

---
+++ assets/js/script.js
@@ -0,0 +1,23 @@
+function runGame() {
+
+}
+
+function checkAnswer() {
+
+}
+
+function incrementScore {
+
+}
+
+function displayQuestion1 {
+
+}
+
+function displayQuestion2 {
+
+}
+
+function displayQuestion3 {
+    
+}
