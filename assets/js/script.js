3db84650b6799bd165fa65c931e0a37e908946a1
MERWYN BARRETTO <merwynbarretto@gmail.com>
2022-07-12 20:58:27 +0000

    Add radio buttons as options and linked to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,51 +1,49 @@
 
-document.addEventListener('DOMContentLoaded', function() {
-    let buttons = document.getElementsByTagName('button');
+document.addEventListener("DOMContentLoaded", function() {
+    let buttons = document.getElementsByTagName("button");
 
     for (let button of buttons) {
-        button.addEventListener('click', function() {
-            let questionType = this.getAttribute('data-type');
-            runGame('questionType');
-        })
+        button.addEventListener("click", function() {
+            if (this.getAttribute("data-type") === "submit") {
+                
+                checkAnswer();
+            } else {
+                let questionType = this.getAttribute("data-type");
+                runGame(questionType);
+            }
+        });
     }
 
-    runGame('question1');
-})
+    runGame("question1");
+
+});
 
 function runGame(questionType) {
+    
 
     if (questionType === 'question1') {
-        displayQuestion1();
+        displayQuestion('Who Plays Iron Man?','Johnny Depp','Robert Downey Jr.','Brad Pitt');  
+    } else if (questionType === 'question2') {
+        displayQuestion('How many Infinity Stones are there?','5','6.','8');
+    } else if (questionType === 'question3') {
+        displayQuestion("Who was able to pick Thor's hammer in End Game?",'Spider Man','Hulk','Captain America');
     } else {
         alert(`Unknown question type: ${questionType}`);
         throw `Unknown question type ${questionType}.Aborting!`;
     }
-
 }
 
 function checkAnswer() {
-
+    console.log("Inside checkAnswer");
 }
 
 function incrementScore() {
 
 }
 
-function displayQuestion1(question1, choice1, choice2, choice3) {
-
-    document.getElementById('question1').textContent = 'Who Plays Iron Man?';
-    document.getElementById('johnny').textContent = 'Johnny Depp';
-    document.getElementById('robert').textContent = 'Robert Downey Jr.';
-    document.getElementById('brad').textContent = 'Brad Pitt';
-    
-
-
-}
-
-function displayQuestion2() {
-
-}
-
-function displayQuestion3() {
-    
+function displayQuestion(question, choice1, choice2, choice3) {
+    document.getElementById('question').textContent = question;
+    document.getElementById('choice1').textContent = choice1;
+    document.getElementById('choice2').textContent = choice2;
+    document.getElementById('choice3').textContent = choice3;
 }
