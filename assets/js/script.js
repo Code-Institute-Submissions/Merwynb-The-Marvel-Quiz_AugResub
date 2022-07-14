d2e71846b9dff56f4cad065859a0a9f4d5510d93
MERWYN BARRETTO <merwynbarretto@gmail.com>
2022-07-14 19:58:15 +0000

    Add code to check answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,9 @@
 
 document.addEventListener("DOMContentLoaded", function() {
     let buttons = document.getElementsByTagName("button");
-
     for (let button of buttons) {
         button.addEventListener("click", function() {
             if (this.getAttribute("data-type") === "submit") {
-                
                 checkAnswer();
             } else {
                 let questionType = this.getAttribute("data-type");
@@ -13,18 +11,14 @@ document.addEventListener("DOMContentLoaded", function() {
             }
         });
     }
-
     runGame("question1");
-
 });
 
 function runGame(questionType) {
-    
-
     if (questionType === 'question1') {
         displayQuestion('Who Plays Iron Man?','Johnny Depp','Robert Downey Jr.','Brad Pitt');  
     } else if (questionType === 'question2') {
-        displayQuestion('How many Infinity Stones are there?','5','6.','8');
+        displayQuestion('How many Infinity Stones are there?','Five','Six','Eight');
     } else if (questionType === 'question3') {
         displayQuestion("Who was able to pick Thor's hammer in End Game?",'Spider Man','Hulk','Captain America');
     } else {
@@ -34,11 +28,27 @@ function runGame(questionType) {
 }
 
 function checkAnswer() {
-    console.log("Inside checkAnswer");
+    var score = 0;
+    if((document.getElementsByTagName("button")[0].getAttribute("data-type") == "question1") && (document.getElementById('choice2').textContent=="Robert Downey Jr.")){
+        console.log("Correct Answer1");
+        score = incrementScore(score);
+    }
+    
+    if((document.getElementsByTagName("button")[1].getAttribute("data-type") == "question2") && (document.getElementById('choice2').textContent=="Six")){
+        console.log("Correct Answer2");
+        score = incrementScore(score);
+    }
+
+    if((document.getElementsByTagName("button")[2].getAttribute("data-type") == "question3") && (document.getElementById('choice3').textContent=="Captain America")){
+        console.log("Correct Answer3");
+        score = incrementScore(score);
+    }
+    console.log("Score"+score);
 }
 
-function incrementScore() {
-
+function incrementScore(score) {
+    score++;
+    return score;
 }
 
 function displayQuestion(question, choice1, choice2, choice3) {
