e0127e93d879e6da9118fabe8b6482f62687fb7b
MERWYN BARRETTO <merwynbarretto@gmail.com>
2022-07-18 17:24:21 +0000

    Recovered data

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,25 +3,46 @@ const gameArea = document.getElementById("game-area");
 const questionElement = document.getElementById('question');
 const answerElement = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-button");
+const scoreLabel = document.getElementById('score');
+const quizLogo = document.getElementById("logo");
+const quizBanner = document.getElementById("banner");
+const quizTitle = document.getElementById("quiz-title")
+const scoreArea = document.getElementById("score-area");
 
-let currentQuestionIndex, score = 0;
+
+let shuffledQuestion, currentQuestionIndex, score = 0;
 
 startButton.addEventListener('click', startGame)
 
+nextButton.addEventListener('click', () => {
+    answerElement.classList.remove('disable-element');
+    scoreLabel.innerText = score;
+    currentQuestionIndex++;
+    setNextQuestion();
+})
+
 function startGame() {
+    answerElement.classList.remove('disable-element');
     startButton.classList.add('hide');
+    quizBanner.classList.add('hide');
+    scoreLabel.innerText = 0;
+    shuffledQuestion = question.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     gameArea.classList.remove('hide');
+    quizLogo.classList.remove('hide');
+    quizTitle.classList.remove('hide');
+    scoreArea.classList.remove('hide');
     setNextQuestion(); 
 }
 
 function setNextQuestion() {
     resetState();
-    displayQuestion(currentQuestionIndex);
+    displayQuestion(shuffledQuestion[currentQuestionIndex]);
 }
 
-function displayQuestion() {
-    questionElement.innerText = question[0].question;
-    question[0].answers.forEach(answer => {
+function displayQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
         const label = document.createElement('label')
         label.innerText = answer.text;
         label.classList.add('choice-container')
@@ -36,13 +57,22 @@ function displayQuestion() {
 
 function selectAnswer(e) {
     const selectedLabel = e.target
-    const correct = selectedLabel.dataset.correct
+    answerElement.classList.add('disable-element');
+    selectedLabel.classList.add('disable-selected-element');
+    const correct = selectedLabel.dataset.correct;
     if(correct) {
         score = score+10;
     }
-    // Array.from(answerElement.children).forEach(label => {
-    //     setStatusClass(label, label.dataset.correct)
-    // }) 
+    if(shuffledQuestion.length > currentQuestionIndex + 1){
+        nextButton.classList.remove('hide')
+        answerElement.classList.add('disabled')
+    } else{
+        startButton.innerText = 'Restart';
+        scoreLabel.innerText = score;
+        score = 0;
+        startButton.classList.remove('hide');
+        nextButton.classList.add('hide');
+    }
     nextButton.classList.remove('.hide');
 }
 
@@ -52,14 +82,6 @@ function resetState() {
         answerElement.removeChild(answerElement.firstChild)
     }
 }
-
-// function setStatusClass(element, correct) {
-//     clearStatusClass(element)
-//     if(correct) {
-//         element.
-//     }
-
-// }
 
 const question = [
     {
@@ -88,70 +110,32 @@ const question = [
             {text: 'Joey tribbiani', correct: false},
             {text: 'Spider Man', correct: false}
         ]
+    },
+    {
+        question: "Where is Captain America from?",
+        answers:[
+            {text: 'New York', correct: false},
+            {text: 'Texas', correct: false},
+            {text: 'Brooklyn', correct: true},
+            {text: 'Seattle', correct: false}
+        ]
+    },
+    {
+        question: "What is Tony Stark's father's name?",
+        answers:[
+            {text: 'Newton Stark', correct: false},
+            {text: 'Harry Stark', correct: false},
+            {text: 'Henry Stark', correct: false},
+            {text: 'Howard Stark', correct: true}
+        ]
+    },
+    {
+        question: "On what planet was the Soul Stone in Infinity War?",
+        answers:[
+            {text: 'Asgard', correct: false},
+            {text: 'Vormir', correct: true},
+            {text: 'Earth', correct: false},
+            {text: 'Xander', correct: false}
+        ]
     }
 ]
-
-
-// document.addEventListener("DOMContentLoaded", function() {
-//     // let buttons = document.getElementsByTagName("button");
-//     // for (let button of buttons) {
-//         runGame("question1");
-//         button.addEventListener("click", function() {
-//             if (this.getAttribute("data-type") === "submit") {
-//                 checkAnswer();
-//             }
-//             // } else {
-//             //     let questionType = this.getAttribute("data-type");
-//             //     checkAnswer();
-//             //     runGame(questionType);
-//             // }
-//         });
-//     // }
-//     runGame("question1");
-// });
-
-// function runGame(questionType) {
-//     if (questionType === 'question1') {
-//         displayQuestion('Who Plays Iron Man?','Johnny Depp','Robert Downey Jr.','Brad Pitt');  
-//     } else if (questionType === 'question2') {
-//         displayQuestion('How many Infinity Stones are there?','Five','Six','Eight');
-//     } else if (questionType === 'question3') {
-//         displayQuestion("Who was able to pick Thor's hammer in End Game?",'Spider Man','Hulk','Captain America');
-//     } else {
-//         alert(`Unknown question type: ${questionType}`);
-//         throw `Unknown question type ${questionType}.Aborting!`;
-//     }
-// }
-
-// function checkAnswer() {
-   
-//    var score = 0;
-//     if((document.getElementsByTagName("button")[0].getAttribute("data-type") == "question1") && (document.getElementById('choice2').textContent=="Robert Downey Jr.")){
-//         console.log("Correct Answer1");
-//         score = incrementScore(score);
-//     }
-//     console.log("*************"+document.querySelectorAll('input[name="radios"]')[0].value);
-//     if((document.getElementsByTagName("button")[1].getAttribute("data-type") == "question2") && (document.getElementById('choice2').textContent=="Six")){
-//         console.log("Correct Answer2");
-//         score = incrementScore(score);
-//     }
-
-//     if((document.getElementsByTagName("button")[2].getAttribute("data-type") == "question3") && (document.getElementById('choice3').textContent=="Captain America")){
-//         console.log("Correct Answer3");
-//         score = incrementScore(score);
-//     }
-//     document.getElementById('score').textContent = score;
-//     console.log("Score"+score);
-// }
-
-// function incrementScore(score) {
-//     score = score+10;
-//     return score;
-// }
-
-// function displayQuestion(question, choice1, choice2, choice3) {
-//     document.getElementById('question').textContent = question;
-//     document.getElementById('choice1').textContent = choice1;
-//     document.getElementById('choice2').textContent = choice2;
-//     document.getElementById('choice3').textContent = choice3;
-// }
